e6debb40c179cd251c12cc477fc66ff8ebb8ae9b
AATIF GODIL <aatifgodil@gmail.com>
2025-07-31 16:31:03 +0000

    it was working just a while ago, try changing the screener widget to som

--- src/components/shared/trading-view-screener.tsx
+++ src/components/shared/trading-view-screener.tsx
@@ -14,17 +14,42 @@ const TradingViewScreenerWidget: React.FC = () => {
     screenerContainer.innerHTML = '';
 
     const script = document.createElement("script");
-    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-screener.js";
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js";
     script.type = "text/javascript";
     script.async = true;
     script.innerHTML = JSON.stringify({
+      "colorTheme": theme,
+      "dateRange": "12M",
+      "showChart": true,
+      "locale": "en",
       "width": "100%",
       "height": "100%",
-      "defaultColumn": "overview",
-      "screener_type": "stock_mkt",
-      "displayCurrency": "USD",
-      "colorTheme": theme,
-      "locale": "en",
+      "showSymbolLogo": true,
+      "showFloatingTooltip": false,
+      "tabs": [
+        {
+          "title": "Stocks",
+          "symbols": [
+            { "s": "NASDAQ:AAPL", "d": "Apple Inc." },
+            { "s": "NASDAQ:GOOGL", "d": "Alphabet Inc." },
+            { "s": "NASDAQ:MSFT", "d": "Microsoft Corp." },
+            { "s": "NASDAQ:AMZN", "d": "Amazon.com, Inc." },
+            { "s": "NASDAQ:TSLA", "d": "Tesla, Inc." },
+            { "s": "NASDAQ:NVDA", "d": "NVIDIA Corp." }
+          ],
+          "originalTitle": "Stocks"
+        },
+        {
+            "title": "Indices",
+            "symbols": [
+              { "s": "FOREXCOM:SPXUSD", "d": "S&P 500" },
+              { "s": "FOREXCOM:NSXUSD", "d": "US 100" },
+              { "s": "FOREXCOM:DJI", "d": "Dow 30" },
+              { "s": "INDEX:VIX", "d": "VIX" }
+            ],
+            "originalTitle": "Indices"
+        }
+      ]
     });
 
     screenerContainer.appendChild(script);
