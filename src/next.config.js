bb67bd41cd96e26accce6dec46f1ecefe8cf69f0
AATIF GODIL <aatifgodil@gmail.com>
2025-08-15 18:16:57 +0000

    because it says accounts.google.com refused to connect

---
+++ src/next.config.js
@@ -0,0 +1,38 @@
+
+require('dotenv').config({ path: './.env' });
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  experimental: {
+    // This allows the Next.js dev server to accept requests from the
+    // Firebase Studio environment.
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
