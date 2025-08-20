9cab41e55f454a2a2f17ec2228c8dacd25dbde26
AATIF GODIL <aatifgodil@gmail.com>
2025-08-20 20:36:02 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,9 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      }
-    ],
+  experimental: {
+    // This is required to allow the Next.js dev server to accept requests from the browser
+    // in the Firebase Studio environment.
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
   },
 };
 
