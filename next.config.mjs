64ae7bcb22c5a609aa8c2603e948ae665cb75bd0
AATIF GODIL <aatifgodil@gmail.com>
2025-08-21 17:41:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
-  experimental: {
-    // This is required for the app to be able to run in IDX which is a cloud-based IDE.
-    // This allows all origins to access the dev server.
-    allowedDevOrigins: ['*'],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            }
+        ]
+    },
+    experimental: {
+        allowedDevOrigins: [
+            '*.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev'
+        ]
+    }
 };
 
 export default nextConfig;
