55134e45922b776f7b05ce1fde543380cbfb584b
AATIF GODIL <aatifgodil@gmail.com>
2025-08-24 14:28:11 +0000

    cant scroll down fully in explore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,9 +42,7 @@ export default function RootLayout({
           <div className="flex flex-col h-screen">
             <AuthProvider>
               <MainContent>
-                <div className="flex-1 overflow-y-auto">
-                  {children}
-                </div>
+                {children}
               </MainContent>
             </AuthProvider>
             {showBottomNav && <BottomNav />}
--- src/components/layout/main-content.tsx
+++ src/components/layout/main-content.tsx
@@ -23,5 +23,5 @@ export default function MainContent({ children }: MainContentProps) {
     );
   }
 
-  return <>{children}</>;
+  return <div className="flex-1 overflow-y-auto">{children}</div>;
 }
