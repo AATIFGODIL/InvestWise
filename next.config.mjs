0fddadc5e6c9a4df0edf2272dfbb2ada6a5ebb44
AATIF GODIL <aatifgodil@gmail.com>
2025-08-26 17:38:58 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,23 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
   webpack: (config, { isServer }) => {
     // This is to fix a bug with genkit and handlebars
     if (isServer) {
-      config.externals.push('handlebars');
+        config.externals = [...config.externals, 'handlebars'];
     }
-    return config;
+    return config
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
 
-import { redirect } from "next/navigation";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/hooks/use-auth";
+import Loading from "./loading";
 
 // Redirect to the sign-in page by default.
 export default function Home() {
-  redirect('/auth/signin');
+  const router = useRouter();
+  const { user, hydrating } = useAuth();
+
+  useEffect(() => {
+    if (!hydrating) {
+      if (user) {
+        router.push('/dashboard');
+      } else {
+        router.push('/auth/signin');
+      }
+    }
+  }, [user, hydrating, router]);
+  
+  return <Loading />;
 }
