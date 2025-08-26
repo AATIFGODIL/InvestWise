670c4567738a82a23e64b64f534ffd87fb4f18d3
AATIF GODIL <aatifgodil@gmail.com>
2025-08-26 17:42:55 +0000

    The `npm install` command failed in my project. Analyze the following er

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,23 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('handlebars');
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
     ],
-  },
-  webpack: (config, { isServer }) => {
-    // This is to fix a bug with genkit and handlebars
-    if (isServer) {
-        config.externals = [...config.externals, 'handlebars'];
-    }
-    return config
   },
 };
 
