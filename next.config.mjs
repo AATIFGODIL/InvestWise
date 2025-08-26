15cbeaf81d2ec21b2d165ddc6b4ef8493ba59de8
AATIF GODIL <aatifgodil@gmail.com>
2025-08-26 17:38:19 +0000

    Error: Invalid src prop (https://images.unsplash.com/photo-1573164713988

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // genkit, a dependency of this app, depends on handlebars, which has a
-  // dependency on the `require.extensions` webpack feature. This is being
-  // deprecated, and the nextjs team recommends adding it to the list of
-  // server-side external packages to avoid breaking the build.
-  //
-  // See: https://github.com/firebase/genkit/issues/105
-  serverComponentsExternalPackages: ['handlebars'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // This is to fix a bug with genkit and handlebars
+    if (isServer) {
+      config.externals.push('handlebars');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
