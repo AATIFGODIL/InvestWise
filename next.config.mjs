9290b5537a8a45bedc23d9f27902d2e70d34388b
AATIF GODIL <aatifgodil@gmail.com>
2025-08-26 10:28:49 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    // Required for genkit to work.
-    config.externals.push('node:stream/web');
-    return config;
-  },
+  experimental: {},
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
-  experimental: {
-    // This is to allow requests from the firebase studio preview URL.
-    allowedDevOrigins: [
-      '*.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev',
-    ],
+  devServer: {
+    allowedHosts: ["*.cloudworkstations.dev"],
   },
 };
-
 export default nextConfig;
