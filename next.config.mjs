736a8953277a87b328dde7557c471abacd3128a4
AATIF GODIL <aatifgodil@gmail.com>
2025-08-26 16:20:44 +0000

    ill add the photos where should i add and what name for respective chann

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-            },
-            {
-                protocol: 'https',
-                hostname: 'yt3.googleusercontent.com',
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
