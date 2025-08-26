b7f7c92a08389572bf26291e750bee4f84f53a44
AATIF GODIL <aatifgodil@gmail.com>
2025-08-26 17:36:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // This is to fix a bug with handlebars and webpack
-    // https://github.com/handlebars-lang/handlebars.js/issues/1953
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: 'javascript/auto',
-    });
-    return config;
-  },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            // This is the workaround for the handlebars issue.
+            config.externals.push('handlebars');
+        }
+        return config;
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
