36d5aa8b5a0ed17ef5bd1dca9badafd1ce2cf7d0
AATIF GODIL <aatifgodil@gmail.com>
2025-08-26 17:31:17 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-    ],
-  },
+  // genkit, a dependency of this app, depends on handlebars, which has a
+  // dependency on the `require.extensions` webpack feature. This is being
+  // deprecated, and the nextjs team recommends adding it to the list of
+  // server-side external packages to avoid breaking the build.
+  //
+  // See: https://github.com/firebase/genkit/issues/105
+  serverComponentsExternalPackages: ['handlebars'],
 };
 
 export default nextConfig;
