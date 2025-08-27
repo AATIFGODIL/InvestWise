3d5a11b2687c51bcca9e6ee7ffa6d81b69cbe85f
AATIF GODIL <aatifgodil@gmail.com>
2025-08-27 19:26:21 +0000

    this. it has to be this .

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -7,27 +7,16 @@ interface LogoProps {
 export default function Logo({ className }: LogoProps) {
   return (
     <svg
-      width="32"
+      width="42"
       height="32"
-      viewBox="0 0 32 32"
+      viewBox="0 0 42 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={cn("text-primary", className)}
     >
       <path
-        d="M4 7L10 25L16 7L22 25L28 7"
-        stroke="currentColor"
-        strokeWidth="3.5"
-        strokeLinejoin="round"
-      />
-      <line
-        x1="16"
-        y1="0"
-        x2="16"
-        y2="32"
-        stroke="#4C1D95"
-        strokeWidth="3.5"
-        strokeLinecap="round"
+        d="M5.33333 12H11.3333V28H5.33333V12ZM8.33333 0C10.5417 0 12.3333 1.79167 12.3333 4C12.3333 6.20833 10.5417 8 8.33333 8C6.125 8 4.33333 6.20833 4.33333 4C4.33333 1.79167 6.125 0 8.33333 0ZM15.3333 28H21.4583L28.3333 10.9583L35.2083 28H41.3333L31.3333 8.33333C30.25 6.45833 28.3333 5.33333 26.2917 5.33333C24.25 5.33333 22.3333 6.45833 21.25 8.33333L15.3333 18.0417V28Z"
+        fill="currentColor"
       />
     </svg>
   );
