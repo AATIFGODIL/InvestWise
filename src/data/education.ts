ffb8b31ae54a12f24c626b42b39a03c0ad9345bf
AATIF GODIL <aatifgodil@gmail.com>
2025-08-27 12:44:10 +0000

    market structures : infographic.png in public folder, compound interest

--- src/data/education.ts
+++ src/data/education.ts
@@ -3,13 +3,13 @@ export const educationalContent = [
   {
     title: "Market Structures Explained",
     description: "A finance infographic explaining different market structures.",
-    filePath: "https://picsum.photos/600/400",
+    filePath: "/infographic.png",
     type: 'image',
   },
   {
     title: "The Power of Compound Interest",
     description: "A finance infographic explaining compound interest.",
-    filePath: "https://picsum.photos/600/400",
+    filePath: "/deliverables.png",
     type: 'image',
   }
 ];
