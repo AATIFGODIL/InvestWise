ed69c189c7ee6e2f4e7d75dac2a213374dae7f66
AATIF GODIL <aatifgodil@gmail.com>
2025-09-24 17:25:08 +0000

    rounded from top and bottom

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -34,7 +34,7 @@ export default function Header() {
   const { username, photoURL } = useUserStore();
 
   return (
-    <header className="sticky top-0 z-30 flex h-20 items-center justify-between border-b bg-primary px-4 text-primary-foreground sm:px-6">
+    <header className="sticky top-0 z-30 flex h-20 items-center justify-between border-b bg-primary px-4 text-primary-foreground sm:px-6 rounded-t-2xl rounded-b-2xl">
       <div className="flex items-center gap-2 font-semibold">
         <Link href="/dashboard" className="flex items-center gap-2">
           <h1 className="text-xl font-bold text-primary-foreground">InvestWise</h1>
