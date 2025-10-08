1f89533fe0867bcf0155d2411d2985b21c09c66c
AATIF GODIL <aatifgodil@gmail.com>
2025-10-08 20:01:23 +0000

    where are the changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'static2.finnhub.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'static2.finnhub.io',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            },
+             {
+                protocol: 'https',
+                hostname: 'image.cnbcfm.com',
+            }
+        ],
+    },
 };
 
 export default nextConfig;
