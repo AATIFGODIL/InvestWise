02b4ac47658bc58a242a0747f3b51ac5dd550e80
AATIF GODIL <aatifgodil@gmail.com>
2025-10-30 12:33:28 +0000

    You've correctly identified the problem. Your description is perfect: "t

--- src/components/auth/animated-border.tsx
+++ src/components/auth/animated-border.tsx
@@ -14,7 +14,16 @@ export default function AnimatedBorder({ className }: AnimatedBorderProps) {
     const height = 532;
     const radius = 12;
 
-    const halfPathLength = (width / 2 - radius) + (height - 2 * radius) + (width / 2 - radius) + (Math.PI * radius);
+    // Path 'd' uses an arc radius of 'radius - 1'.
+    const arcRadius = radius - 1; 
+    // Two 90-deg arcs = one 180-deg arc (PI * r)
+    const arcLength = Math.PI * arcRadius; 
+    const topBottomLength = (width / 2) - radius;
+    // Straight vertical side: (height - radius - 1) - (radius + 1) = height - 2*radius - 2
+    const sideLength = height - 2 * radius - 2;
+
+    // The total length of one half-path
+    const halfPathLength = topBottomLength + sideLength + topBottomLength + arcLength;
 
     return (
         <div
@@ -28,32 +37,33 @@ export default function AnimatedBorder({ className }: AnimatedBorderProps) {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox={`0 0 ${width} ${height}`}
+                preserveAspectRatio="none"
             >
                 {/* Left Path */}
                 <path
-                    d={`M${width/2},1 H${radius} A${radius-1} ${radius-1} 0 0 0 1,${radius+1} V${height-radius-1} A${radius-1} ${radius-1} 0 0 0 ${radius},${height-1} H${width/2}`}
+                    d={`M${width/2},1 H${radius} A${arcRadius} ${arcRadius} 0 0 0 1,${radius+1} V${height-radius-1} A${arcRadius} ${arcRadius} 0 0 0 ${radius},${height-1} H${width/2}`}
                     className="stroke-primary animated-border-path"
                     strokeWidth="2"
                     strokeLinecap="round"
                     style={{
+                        '--path-length': halfPathLength,
                         strokeDasharray: halfPathLength,
-                        strokeDashoffset: halfPathLength,
                         filter: isClearMode ? 'drop-shadow(0 0 3px hsl(var(--primary)))' : 'none',
                         vectorEffect: "non-scaling-stroke",
-                    }}
+                    } as React.CSSProperties}
                 />
                  {/* Right Path */}
                 <path
-                    d={`M${width/2},1 H${width-radius} A${radius-1} ${radius-1} 0 0 1 ${width-1},${radius+1} V${height-radius-1} A${radius-1} ${radius-1} 0 0 1 ${width-radius},${height-1} H${width/2}`}
+                    d={`M${width/2},1 H${width-radius} A${arcRadius} ${arcRadius} 0 0 1 ${width-1},${radius+1} V${height-radius-1} A${arcRadius} ${arcRadius} 0 0 1 ${width-radius},${height-1} H${width/2}`}
                     className="stroke-primary animated-border-path"
                     strokeWidth="2"
                     strokeLinecap="round"
                     style={{
+                        '--path-length': halfPathLength,
                         strokeDasharray: halfPathLength,
-                        strokeDashoffset: halfPathLength,
                         filter: isClearMode ? 'drop-shadow(0 0 3px hsl(var(--primary)))' : 'none',
                         vectorEffect: "non-scaling-stroke",
-                    }}
+                    } as React.CSSProperties}
                 />
             </svg>
         </div>
