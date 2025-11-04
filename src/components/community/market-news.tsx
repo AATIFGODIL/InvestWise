49fedc87a30c57c5c53b2c439211e68538bd7ac1
AATIF GODIL <aatifgodil@gmail.com>
2025-11-04 17:07:45 +0000

    feat: Add market news component

---
+++ src/components/community/market-news.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MarketNews = () => {
+  return (
+    <div>
+      {/* Placeholder for Market News */}
+      <h2>Market News</h2>
+      <p>Coming soon...</p>
+    </div>
+  );
+};
+
+export default MarketNews;
