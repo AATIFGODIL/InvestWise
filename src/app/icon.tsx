5f416bce7006a36c10c06c33a441a129a70052b8
AATIF GODIL <aatifgodil@gmail.com>
2025-12-03 12:30:36 +0000

    style: Correct W shape in app icon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,78 +1,89 @@
 import { ImageResponse } from 'next/og'
  
-// Image metadata
 export const size = {
   width: 32,
   height: 32,
 }
 export const contentType = 'image/png'
  
-// Image generation
 export default function Icon() {
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
-          fontSize: 24,
-          background: '#775DEF', // Signature purple background
           width: '100%',
           height: '100%',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          color: 'white',
-          borderRadius: '20%', // Rounded corners for icon look
-          fontWeight: 900,
-          fontFamily: 'sans-serif',
+          background: 'transparent',
           position: 'relative',
         }}
       >
-        <div style={{ position: 'relative', width: '24px', height: '24px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            {/* The "I" acting as a vertical line of symmetry */}
-            <div style={{ 
-                position: 'absolute', 
-                left: '50%', 
-                transform: 'translateX(-50%)', 
-                height: '80%', 
-                width: '3px', 
-                background: 'white',
-                borderRadius: '1px'
-            }}></div>
-            
-            {/* The "W" parts - Symmetric around the I */}
-            <div style={{ width: '100%', height: '80%', display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end' }}>
-                {/* Left arm of W */}
+        {/* The W parts */}
+        <div style={{ 
+            display: 'flex', 
+            height: '60%', 
+            width: '100%', 
+            justifyContent: 'center',
+            position: 'absolute',
+            bottom: '20%',
+        }}>
+            {/* Left V of the W */}
+            <div style={{ display: 'flex', alignItems: 'flex-end', marginRight: '-1px' }}>
                 <div style={{ 
-                    width: '3px', 
+                    width: '4px', 
                     height: '100%', 
-                    background: 'white', 
-                    borderRadius: '1px',
-                    transform: 'rotate(-15deg)',
-                    transformOrigin: 'bottom right',
-                    marginRight: '2px'
+                    background: '#775DEF', 
+                    transform: 'skewX(-15deg)', 
+                    borderRadius: '2px'
                 }}></div>
-                
-                {/* Right arm of W */}
                 <div style={{ 
-                    width: '3px', 
+                    width: '4px', 
                     height: '100%', 
-                    background: 'white', 
-                    borderRadius: '1px',
-                    transform: 'rotate(15deg)',
-                    transformOrigin: 'bottom left',
-                    marginLeft: '2px'
+                    background: '#775DEF', 
+                    transform: 'skewX(15deg)', 
+                    borderRadius: '2px',
+                    marginLeft: '-2px' // Overlap slightly
                 }}></div>
             </div>
-            {/* Inner legs of W connecting to I */}
-             <div style={{ position: 'absolute', bottom: 0, width: '100%', height: '60%', display: 'flex', justifyContent: 'center', alignItems: 'flex-end' }}>
-                 <div style={{ width: '3px', height: '100%', background: 'white', transform: 'rotate(15deg)', transformOrigin: 'bottom right', marginRight: '1px', borderRadius: '1px' }}></div>
-                 <div style={{ width: '3px', height: '100%', background: 'white', transform: 'rotate(-15deg)', transformOrigin: 'bottom left', marginLeft: '1px', borderRadius: '1px' }}></div>
-             </div>
+
+            {/* Gap for the I */}
+            <div style={{ width: '4px' }}></div>
+
+            {/* Right V of the W */}
+            <div style={{ display: 'flex', alignItems: 'flex-end', marginLeft: '-1px' }}>
+                <div style={{ 
+                    width: '4px', 
+                    height: '100%', 
+                    background: '#775DEF', 
+                    transform: 'skewX(-15deg)', 
+                    borderRadius: '2px'
+                }}></div>
+                <div style={{ 
+                    width: '4px', 
+                    height: '100%', 
+                    background: '#775DEF', 
+                    transform: 'skewX(15deg)', 
+                    borderRadius: '2px',
+                    marginLeft: '-2px' 
+                }}></div>
+            </div>
         </div>
+
+        {/* The I - Taller and on top, vertically centered */}
+        <div style={{ 
+            position: 'absolute', 
+            width: '4px', 
+            height: '90%', 
+            background: '#775DEF', 
+            borderRadius: '2px',
+            zIndex: 10,
+            left: '50%',
+            transform: 'translateX(-50%)'
+        }}></div>
       </div>
     ),
-    // ImageResponse options
     {
       ...size,
     }
