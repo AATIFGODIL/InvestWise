355a6c03b14f8d92e00a53ac6f80c62d5476cb13
AATIFGODIL <aatifgodil@gmail.com>
2026-01-04 11:11:39 +0530

    fix: Add missing storageBucket to Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "investwise-f9rch",
-  "appId": "1:509703968960:web:4920eb3cbfa5d86094f525",
   "apiKey": "AIzaSyAAdBMaAXBV2PSjJr3jzw9obDJcBB3fbhc",
   "authDomain": "investwise-f9rch.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "509703968960"
+  "projectId": "investwise-f9rch",
+  "storageBucket": "investwise-f9rch.firebasestorage.app",
+  "messagingSenderId": "509703968960",
+  "appId": "1:509703968960:web:4920eb3cbfa5d86094f525",
+  "measurementId": ""
 };
